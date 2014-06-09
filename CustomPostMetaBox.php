ee60f2bc843990e1d0e880de3ba6e34519e36745
The Ambassador of Funk <acobster@gmail.com>
2014-06-09 14:55:03 -0700

    make meta box initialization static

--- CustomPostMetaBox.php
+++ CustomPostMetaBox.php
@@ -1,106 +1,130 @@
 <?php
 
 class CustomPostMetaBox {
-    
-    protected $id;
-    
-    protected $postType;
-    
-    protected $fields;
-    
-    public function __construct( $id, $info, $postType ) {
-        $this->id = $id;
-        $this->postType = $postType;
-        $this->info = $info;
-        
+
+    /**
+     * "id" attribute of the meta box within the Admin section
+     * @var string
+     */
+    protected static $id;
+
+    /**
+     * The configuration for this meta box
+     * @var array
+     */
+    protected static $config;
+
+    /**
+     * The custom post type for this meta box
+     * @var string
+     */
+    protected static $postType;
+
+    /**
+     * The custom fields associated with this meta box
+     * @var array
+     */
+    protected static $fields;
+
+    public static function init( $id, $config, $postType ) {
+        self::$id = $id;
+        self::$config = $config;
+        self::$postType = $postType;
+
         // Support either numeric arrays with just field names (which will default
         // to type "text") or associative arrays formatted as name => [ info... ]
-        foreach( $info['fields'] as $name => $field ) {
+        foreach( static::$config['fields'] as $name => $field ) {
             if( is_int( $name ) && is_string( $field ) )
             {
                 $name = $field;
                 $field = array();
             }
-            $this->fields[$name] = CustomPostField::create( $name, $field );
+            self::$fields[$name] = CustomPostField::create( $name, $field );
         }
-    }
-    
-    public function init() {
-        add_action( 'add_meta_boxes', array( $this, 'addMeta' ) );
-        add_action( 'save_post', array( $this, 'saveMeta' ) );
-    }
-    
-    public function addMeta() {
 
-        $priority = isset( $this->info['priority'] )
-            ? $this->info['priority']
+        add_action( 'add_meta_boxes', array( get_called_class(), 'addMeta' ) );
+        add_action( 'save_post', array( get_called_class(), 'saveMeta' ) );
+    }
+
+    /**
+     * Call the add_meta_box WP hook based on class configuration.
+     */
+    public static function addMeta() {
+
+        $priority = isset( static::$config['priority'] )
+            ? static::$config['priority']
             : 'default';
-            
-        $args = isset( $this->info['callback_args'] )
-            ? $this->info['callback_args']
+
+        $args = isset( static::$config['callback_args'] )
+            ? static::$config['callback_args']
             : null;
-            
-        $context = isset( $this->info['context'] )
-            ? $this->info['context']
+
+        $context = isset( static::$config['context'] )
+            ? static::$config['context']
             : 'advanced';
-        
-        add_meta_box( $this->id,
-                      $this->info['title'],
-                      array( $this, 'showMeta' ),
-                      $this->postType,
+
+        add_meta_box( static::$id,
+                      static::$config['title'],
+                      array( get_called_class(), 'showMeta' ),
+                      static::$postType,
                       $context,
                       $priority,
                       $args
                     );
     }
-    
-    
-    
+
+
+
     /************************ MODEL METHODS *************************/
-    
-    public function saveMeta( $postId ) {
-        
-        $nonceName = $this->getNonceFieldName();
-        
-        // Bail if we're doing an auto save, if we can't verify the nonce,
-        // or if the current user does not have the proper privileges
+
+    public static function saveMeta( $postId ) {
+
+        $nonceName = static::getNonceFieldName();
+
+        // Bail if we're doing an auto save,
+        //  if we can't verify the nonce,
+        //  or if the current user does not have the proper privileges
         if( ( defined( 'DOING_AUTOSAVE' ) xor DOING_AUTOSAVE ) &&
-        
+
             ( isset( $_POST[$nonceName] ) ||
               wp_verify_nonce( $_POST[$nonceName], $nonceName ) ) &&
-              
+
             current_user_can( 'edit_post' ) )
         {
-            foreach( $this->fields as $name => $field ) {
+            foreach( static::$fields as $name => $field ) {
                 $value = $field->getSubmitted();
                 update_post_meta( $postId, "_$name", maybe_serialize( $value ) );
             }
         }
     }
-    
-    public function getData( $postId ) {
+
+    public static function getFields() {
+        return static::$fields;
+    }
+
+    public static function getData( $postId ) {
         $data = array();
-        
-        foreach( $this->$fields as $name => $field ) {
+
+        foreach( static::$fields as $name => $field ) {
             $data[$name] = $field->getData();
         }
-        
+
         return $data;
     }
-    
-    
-    
+
+
+
     /************************ VIEW METHODS *************************/
-    
-    public function showMeta( $post ) {
-        $nonce = $this->getNonceFieldName();        
+
+    public static function showMeta( $post ) {
+        $nonce = static::getNonceFieldName();
         wp_nonce_field( $nonce, $nonce );
-        
+
         $html = array();
-        
-        foreach( $this->fields as $field ) {
+
+        foreach( static::$fields as $field ) {
              $field = $field->getMetaHtml( $post->ID );
-             if( ! isset( $this->info['no_formatting'] ) )
+             if( ! isset( static::$config['no_formatting'] ) )
              {
                  $field = "<p>$field</p>";
              }
@@ -109,9 +133,9 @@ class CustomPostMetaBox {
 
         echo implode( "\n", $html );
     }
-    
-    protected function getNonceFieldName() {
-        return "{$this->id}_nonce";
+
+    protected static function getNonceFieldName() {
+        return static::$id.'_nonce';
     }
 }
 
--- CustomPostType.php
+++ CustomPostType.php
@@ -29,22 +29,62 @@ abstract class CustomPostType {
      */
     protected $data;
 
-    public function __construct() {
-        $this->data = array();
+    /**
+     * The actual WP_Post instance for this custom post
+     * @var WP_Post
+     */
+    protected $wp_post;
+
+    public static function fetch( $post, $meta=true ) {
+        $post = get_post( $post );
+        $data = get_post( $post, ARRAY_A );
+
+        if( $meta ) {
+            $metaData = get_post_meta( $post->ID );
+
+            foreach( static::getMetaFieldNames() as $field ) {
+                // We always use "custom" fields, so prefix with "_"
+                // to avoid collisions
+                $fieldName = "_$field";
+                if( ! empty($metaData[$fieldName]) ) {
+                    $data[$fieldName] = $metaData[$fieldName][0];
+                }
+            }
+        }
+
+        $class = get_called_class();
+        $custom = new $class( $post, $data );
+        return $custom;
     }
 
+    /**
+     * Constructor. Calling this directly is discouraged in favor of find()
+     * @param $wp_post the WP_Post object
+     */
+    public function __construct( $wp_post, $data=array() ) {
+        $this->wp_post = $wp_post;
+        $this->post_id = $wp_post->ID;
+        $this->data = $data;
+    }
+
+    /**
+     * Register the custom post type along with all its meta boxes
+     * @param  string $action which WordPress action to hook into
+     */
     public static function init( $action = 'init' ) {
         // Use Late Static Binding to call register() on the subclass,
         // not the CustomPostType class
         add_action( $action, array( get_called_class(), 'register' ) );
 
         // Initialize meta boxes
-        foreach( static::$meta as $id => $info ) {
-            $meta = new CustomPostMetaBox( $id, $info, static::$name );
-            $meta->init();
+        foreach( static::$meta as $id => $config ) {
+            CustomPostMetaBox::init( $id, $config, static::$name );
         }
     }
 
+    /**
+     * Explicitly register the custom post type
+     */
     public static function register() {
         register_post_type( static::$name, static::$args );
     }
